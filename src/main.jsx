013d468667c6179599a21184adfda274c8b7e967
yuri <jyuri1114@naver.com>
2025-05-20 15:49:34 +0900

    [add] 배포완

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={`${import.meta.env.VITE_PUBLIC_URL}`}>
       <App />
     </BrowserRouter>
   </StrictMode>
